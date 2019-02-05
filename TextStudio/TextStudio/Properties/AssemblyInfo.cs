bad6ef92dd509893a262eb9ceb016186614a04a3
TheRealMichaelWang <44185024+TheRealMichaelWang@users.noreply.github.com>
2019-02-05 15:57:34 -0800

    Add files via upload

--- TextStudio/TextStudio/Properties/AssemblyInfo.cs
+++ TextStudio/TextStudio/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TextStudio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("Michael Wang")]
 [assembly: AssemblyProduct("TextStudio")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 Michael Wang")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
